601a59e5673c79741e104be1dc05c6da31511ac1
Egor <egor.zholobov@gmail.com>
2022-11-29 20:39:54 +0300

    сделаны все 3 дз

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        //1
+        person Maxim = new person(1988,"Максим", "Минск","бренд-менеджер");
+        person Anya = new person(1993,"Аня", "Москва","методист образовательных программ");
+        person Katya = new person(1992,"Катя", "Калининград","продакт-менеджер");
+        person Artyom = new person(1995,"", "Москва","директор по развитию бизнеса");
+        System.out.println(Maxim.toString());
+        System.out.println(Anya.toString());
+        System.out.println(Katya.toString());
+        System.out.println(Artyom.toString());
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=");
+        //2
+        car LadaGranta = new car("Lada","Granta",0,"жёлтый",2015,"Россия");
+        car AudiA8 = new car("Audi", "A8", 3.0, "чёрный",2020,"Германия");
+        car BmwZ8 = new car("BMW", "Z8", 3.0,"чёрный",2021,"Германия");
+        car KiaSportage = new car("Kia", "Sportage", 2.4,"красный",2018,"Южная Корея");
+        car HyundaiAvante = new car("Hyundai","Avante",1.6,"оранжевый",2016,"Южная Корея");
+        System.out.println(LadaGranta.toString());
+        System.out.println(AudiA8.toString());
+        System.out.println(BmwZ8.toString());
+        System.out.println(KiaSportage.toString());
+        System.out.println(HyundaiAvante.toString());
 
     }
 }
--- person.java
+++ person.java
@@ -1,3 +1,37 @@
 public class person {
+    final String NO_INFO = "информация не указана";
+    int yearOfBirth;
+    String name;
+    String town;
+    String jobTitle;
 
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. " + "Я работаю на должности " + jobTitle + ". Будем знакомы!";
+    }
+
+    public person(int yearOfBirth, String name, String town, String jobTitle){
+        if(yearOfBirth >= 0){
+            this.yearOfBirth = yearOfBirth;
+        }else {
+            throw new IllegalArgumentException("год рождения не может быть отрицательным");
+        }
+        if(name == null || name.isEmpty()){
+            this.name = NO_INFO;
+        }else {
+            this.name = name;
+        }
+        if(town == null || town.isEmpty()){
+            this.town = NO_INFO;
+        }else {
+            this.town = town;
+        }
+        if(jobTitle == null || jobTitle.isEmpty()){
+            this.jobTitle = NO_INFO;
+        }else {
+            this.jobTitle = jobTitle;
+        }
+
+
+    }
 }
